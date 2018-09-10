be762b888764d6ada8fe375be87b47f39a414bcd
aperezmm <33065509+aperezmm@users.noreply.github.com>
2018-09-10 11:29:56 -0500

    Añadiendo funcionalidades

--- TP1Sakila/TP1Sakila-war/src/java/managedbeans/ActorMBean.java
+++ TP1Sakila/TP1Sakila-war/src/java/managedbeans/ActorMBean.java
@@ -5,9 +5,13 @@
  */
 package managedbeans;
 
+import entities.Actor;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import session.ActorManager;
 
 /**
  *
@@ -17,10 +21,46 @@ import java.io.Serializable;
 @SessionScoped
 public class ActorMBean implements Serializable {
 
+    @EJB
+    private ActorManager actorManager;
+    
+    private Actor actor;
+    private List<Actor> actors;
+
     /**
      * Creates a new instance of ActorMBean
      */
     public ActorMBean() {
+    }
+    
+    public List<Actor> getActors(){
+        if((actors == null || (actors.isEmpty())))
+            refresh();
+        return actors;
+    }
+
+    private void refresh() {
+        actors = actorManager.getAllActors();
+    }
+    
+    public Actor getDetails(){
+        return actor;
+    }
+    
+    public String showDetails(Actor actor){
+        this.actor = actor;
+        return "ActorDetails";
+    }
+    
+    public String update(){
+        System.out.println("###UPDATE###");
+        actor = actorManager.update(actor);
+        return "ActorList";
+    }
+    
+    public String list(){
+        System.out.println("###LIST###");
+        return "CustomerList";
     }
     
 }
